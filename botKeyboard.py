63182aaf9ea2fa35940415816345b98bb4354d5d
Vladislav-Nikita <79220607+Vladislav-Nikita@users.noreply.github.com>
2023-08-04 15:29:40 +0300

    Update botKeyboard.py

--- botKeyboard.py
+++ botKeyboard.py
@@ -9,7 +9,7 @@ menu = {}
 allDishes = {}
 remove_mode = 0
 
-with open('menu.txt', 'r') as f:
+with open('menu.txt','r', encoding = 'utf-8') as f:
     lines = f.readlines()
     category = ''
     for line in lines:
@@ -28,9 +28,12 @@ for i in dishies:
 
 
 def create_menu(message_text=None, user_id=None):
+    back = types.KeyboardButton('Back')
+    
     if message_text is None and user_id is None:  # Create start menu
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=4)
         btn_arr = []
+        
         for m in menu:
             btn_arr.append(types.KeyboardButton(text=f'{m}'))
         markup.add(*btn_arr)
@@ -42,7 +45,7 @@ def create_menu(message_text=None, user_id=None):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         for el in menu[message_text]:
             markup.add(types.KeyboardButton(text=f'{el} - {menu[message_text][el]}'))
-        back = types.KeyboardButton('Back')
+
         markup.add(back)
         return markup
     else:  # Create remove mode menu
@@ -51,7 +54,7 @@ def create_menu(message_text=None, user_id=None):
         for m in user_orders[user_id]:
             btn_arr.append(types.KeyboardButton(text=f'{m}    x{user_orders[user_id][m]}'))
         markup.add(*btn_arr)
-        markup.add(types.KeyboardButton(text='Back'))
+        markup.add(back)
         return markup
 
 
@@ -59,12 +62,12 @@ def create_order_msg(message):
     global user_orders, user_prices, allDishes
     msg = 'Your order:\n'
     total_price = 0
-    for dish in user_orders[message.from_user.username]:
-        msg += f'{dish} -- {allDishes[dish]}p.  x{user_orders[message.from_user.username][dish]}\n'
-        total_price += allDishes[dish] * user_orders[message.from_user.username][dish]
+    for dish in user_orders[message.from_user.id]:
+        msg += f'{dish} -- {allDishes[dish]}p.  x{user_orders[message.from_user.id][dish]}\n'
+        total_price += allDishes[dish] * user_orders[message.from_user.id][dish]
         total_price = round(total_price, 2)
     msg += f'\nTotal price: {total_price}p.'
-    user_prices.update({message.from_user.username: total_price})
+    user_prices.update({message.from_user.id: total_price})
     return msg
 
 
@@ -72,7 +75,6 @@ def create_order_msg(message):
 def start(message):
     bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name}",
                      reply_markup=create_menu())
-    # bot.send_message(message, f'Привет, {message.from_user}',reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
@@ -82,42 +84,45 @@ def bot_message(message):
     if message.chat.type == 'private':
         if message.text in menu:
             bot.send_message(message.chat.id, message.text, reply_markup=create_menu(message_text=message.text))
+
         elif message.text == 'Back':
             if remove_mode == 1:
                 remove_mode = 0
             bot.send_message(message.chat.id, 'Back', reply_markup=create_menu())
+
         elif message.text.split('-')[0].strip() in allDishes:
             dish_name = message.text.split('-')[0].strip()
-            if message.from_user.username in user_orders:
-                if dish_name in user_orders[message.from_user.username]:
-                    user_orders[message.from_user.username][dish_name] += 1
+            if message.from_user.id in user_orders:
+                if dish_name in user_orders[message.from_user.id]:
+                    user_orders[message.from_user.id][dish_name] += 1
                 else:
-                    user_orders[message.from_user.username].update({dish_name: 1})
+                    user_orders[message.from_user.id].update({dish_name: 1})
             else:
-                user_orders.update({message.from_user.username: {dish_name: 1}})
+                user_orders.update({message.from_user.id: {dish_name: 1}})
             bot.send_message(message.chat.id, f'{dish_name} added')
             bot.send_message(message.chat.id, create_order_msg(message))
 
         elif message.text.split('x')[0].strip() in allDishes:
             dish_name = message.text.split('x')[0].strip()
-            user_orders[message.from_user.username][dish_name] -= 1
-            if user_orders[message.from_user.username][dish_name] == 0:
-                del user_orders[message.from_user.username][dish_name]
-            if user_orders[message.from_user.username] == {}:
-                del user_orders[message.from_user.username]
-                del user_prices[message.from_user.username]
+            user_orders[message.from_user.id][dish_name] -= 1
+            if user_orders[message.from_user.id][dish_name] == 0:
+                del user_orders[message.from_user.id][dish_name]
+            if user_orders[message.from_user.id] == {}:
+                del user_orders[message.from_user.id]
+                del user_prices[message.from_user.id]
             bot.send_message(message.chat.id, f'1 {dish_name} deleted',
-                             reply_markup=create_menu(user_id=message.from_user.username))
+                             reply_markup=create_menu(user_id=message.from_user.id))
             bot.send_message(message.chat.id, create_order_msg(message))
+
         elif message.text == 'Clear all':
-            del user_orders[message.from_user.username]
-            del user_prices[message.from_user.username]
+            del user_orders[message.from_user.id]
+            del user_prices[message.from_user.id]
             bot.send_message(message.chat.id, 'Cleared!')
 
         elif message.text == 'Remove mode':
             remove_mode = 1
             bot.send_message(message.chat.id, 'Remove something!',
-                             reply_markup=create_menu(user_id=message.from_user.username))
+                             reply_markup=create_menu(user_id=message.from_user.id))
 
         elif message.text == 'Confirm order':
             f = open('orders.txt', 'w')
@@ -128,6 +133,7 @@ def bot_message(message):
                 f.write(f'Total: {user_prices[user]}')
                 f.write('\n_______________\n')
             bot.send_message(message.chat.id, 'Order confirmed!')
+            
         else:
             bot.send_message(message.chat.id, 'Something wrong')
 
